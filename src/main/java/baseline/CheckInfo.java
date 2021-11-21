98b3f3640843cd5ce0342acfed55c06744d0175f
Fareed <fareedally1999@gmail.com>
2021-11-21 11:16:03 -0500

    updated Item class and checkInfo class to validate all info

---
+++ src/main/java/baseline/CheckInfo.java
@@ -0,0 +1,46 @@
+package baseline;
+
+// Purpose of this class is to validate all the info the user enters for a new item
+public class CheckInfo
+{
+    public boolean checkName(String name)
+    {
+        // Check if length of name is between 2 and 256
+        return name.length() >= 2 && name.length() <= 256;
+    }
+
+    public boolean checkValue(float value)
+    {
+        // Check if value is greater than or equal to 0
+        return value >= 0;
+    }
+
+    public boolean checkSerialNumber(String serialNumber)
+    {
+        // First check to see if it has the correct length
+        char [] temp = serialNumber.toCharArray();
+
+        if(temp.length != 13)
+        {
+            return false;
+        }
+
+        // Check to see if they have the correct dashes and letters/numbers
+        for(int i=0; i<13; i++) {
+            if (Character.isDigit(temp[0])) {
+                // Checks if first index is a letter
+                return false;
+            }
+
+            // Checks if dashes are in correct location
+            if ((i == 1 || i == 5 || i == 9) && temp[i] != '-') {
+                return false;
+            }
+
+        }
+
+        // Check to see if special characters are present
+        return false;
+    }
+
+}
---
+++ src/main/java/baseline/Item.java
@@ -0,0 +1,82 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item
+{
+    private String name;
+    private float value;
+    private String serialNumber;
+
+    // Create check object to validate info
+    private final CheckInfo check = new CheckInfo();
+
+    // Create list to hold all items
+    private static final List<Item> inventory = new ArrayList<>();
+
+    // Constructor
+    public Item(String name, float value, String serialNumber)
+    {
+        this.name = name;
+        this.value = value;
+
+        // Check serialNumber then assign it
+        if(check.checkSerialNumber(serialNumber))
+        {
+            this.serialNumber = serialNumber;
+        }
+        else if(serialNumber.equals("") || !check.checkSerialNumber(serialNumber))
+        {
+            this.serialNumber = "Enter a valid serial number";
+        }
+
+    }
+
+    public void setName(String name)
+    {
+        // Check name then assign it
+        if(check.checkName(name))
+        {
+            this.name = name;
+        }
+    }
+
+    public void setValue(float value)
+    {
+        // Check value then assign it
+        if(check.checkValue(value))
+        {
+            this.value = value;
+        }
+    }
+
+    public void setSerialNumber(String serialNumber)
+    {
+        // Check serial number then assign it
+        if(check.checkSerialNumber(serialNumber))
+        {
+            this.serialNumber = serialNumber;
+        }
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getSerialNumber()
+    {
+        return serialNumber;
+    }
+
+    public float getValue()
+    {
+        return value;
+    }
+
+    public static List<Item> getInventory()
+    {
+        return inventory;
+    }
+}
